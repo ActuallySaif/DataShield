6c7b89ba554a7f65311bbc1cd80f370704ccd7bf
saifaldin hossam <actually.saif@gmail.com>
2025-12-23 06:34:16 +0200

    login js done

--- login.js
+++ login.js
@@ -1,10 +1,18 @@
-let email = document.getElementById("email").value;
-let pass = document.getElementById("pass").value;
+
+let errMsg = document.getElementById("errMsg");
 
 let localusers = JSON.parse(localStorage.getItem("localUsers"));
 
-for (i = 0; i < localusers.length; i++) {
-    if (email == localusers[i].email && pass == localusers[i].pass) {
-        window.location.href = 'index.html'
+
+function loginCheck() {
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+     
+    for (i = 0; i < localusers.length; i++) {
+        if (email == localusers[i].email && pass == localusers[i].pass) {
+            window.location.href = 'index.html'
+        } else {
+            errMsg.style.display = 'block'
+        }
     }
 }
